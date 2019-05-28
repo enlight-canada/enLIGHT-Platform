08cec24c9b7d0cb24da0154a9c1eba0090fefba2
Nathan Li <n63li@edu.uwaterloo.ca>
2019-05-27 21:06:28 -0400

    test

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a test
         </a>
       </header>
     </div>
